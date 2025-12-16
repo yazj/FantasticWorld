00483659a479e3d638d27cbd872ce7be8e49452c
Yuanyi <yuanyi339@gmail.com>
2025-12-16 19:58:35 +0800

    初始化项目文件

---
+++ Source/FantasticWorld/FantasticWorldGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FantasticWorldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FANTASTICWORLD_API AFantasticWorldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
